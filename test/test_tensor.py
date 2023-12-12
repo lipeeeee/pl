51085d71719abe42dd37546dfb58464c300d35d4
lipeeeee <filipe.pereirafga@gmail.com>
2023-12-12 23:08:33 +0000

    Test workflow... probably won't work

--- test/test_tensor.py
+++ test/test_tensor.py
@@ -1,5 +1,5 @@
 # Test pytorch tensors
- 
+
 import random
 import unittest
 import torch
@@ -15,7 +15,7 @@ class TestTensor(unittest.TestCase):
     def test_shape(self):
         a = torch.empty(2, 2, 3)
         self.assertTrue(a.shape == torch.Size([2, 2, 3]))
-        
+
         x, y, z = random.randint(1, 100), random.randint(1, 100), random.randint(1, 100)
         b = torch.zeros(x, y, z)
         self.assertTrue(b.shape == torch.Size([x, y, z]))
